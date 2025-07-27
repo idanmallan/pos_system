bbddf684a430573be8fbb0cf2716abd49498d096
idanmallan <ibashirdanmallan1572@gmail.com>
2025-07-27 16:57:19 +0100

    Added login system

--- pos_system/urls.py
+++ pos_system/urls.py
@@ -4,10 +4,10 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # <-- Add this line
     path('', views.dashboard, name='dashboard'),
     path('items/', views.items, name='items'),
     path('reports/', views.reports, name='reports'),
     path('pos/', views.pos, name='pos'),
     path('receipt/<int:sale_id>/', views.receipt, name='receipt'),
+    path('', include('django.contrib.auth.urls')),  # <-- Add this
 ]
